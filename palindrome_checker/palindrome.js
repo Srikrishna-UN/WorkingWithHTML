b6d355a875650b0f0c7565601bd32c3b1d5872d6
Srikrishna-RVU <srikrishnaun.btech22@rvy.edu.in.com>
2023-04-29 00:12:34 +0530

    New commit 1. Added new file for external js 2. Wrote basic starting HTML structure with some CSS

---
+++ palindrome_checker/palindrome.js
@@ -0,0 +1,10 @@
+function palindrome_checker()
+{
+    let num=Integer.toString(document.getElementById("input").value);
+   let length=num.length;
+
+   for(let i=0;i<parseInt(length/2);i++)
+   {
+    
+   }
+}
